b7a1933c33bb00fa731367bee41c035d4b7855d1
Oreecaa <oreecaa@gmail.com>
2024-05-21 23:07:00 +0700

    - update mvvm block

---
+++ android/src/main/java/com/pvs/flutter_utils/NativeAdFactoryExample.java
@@ -0,0 +1,41 @@
+package com.pvs.flutter_utils;
+
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+
+import com.google.android.gms.ads.nativead.NativeAd;
+import com.google.android.gms.ads.nativead.NativeAdView;
+import com.pvs.flutter_utils.R;
+
+import java.util.Map;
+
+import io.flutter.plugins.googlemobileads.GoogleMobileAdsPlugin;
+import io.flutter.plugins.googlemobileads.GoogleMobileAdsPlugin;
+
+class NativeAdFactoryExample implements GoogleMobileAdsPlugin.NativeAdFactory {
+    private final LayoutInflater layoutInflater;
+
+    NativeAdFactoryExample(LayoutInflater layoutInflater) {
+        this.layoutInflater = layoutInflater;
+    }
+
+    @Override
+    public NativeAdView createNativeAd(
+            NativeAd nativeAd, Map<String, Object> customOptions) {
+        final NativeAdView adView =
+                (NativeAdView) layoutInflater.inflate(R.layout.my_native_ad , null);
+        final TextView headlineView = adView.findViewById(R.id.ad_headline);
+        final TextView bodyView = adView.findViewById(R.id.ad_body);
+
+        headlineView.setText(nativeAd.getHeadline());
+        bodyView.setText(nativeAd.getBody());
+
+        adView.setBackgroundColor(Color.YELLOW);
+
+        adView.setNativeAd(nativeAd);
+        adView.setBodyView(bodyView);
+        adView.setHeadlineView(headlineView);
+        return adView;
+    }
+}
