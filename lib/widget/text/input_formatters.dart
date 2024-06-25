33dfeb9b0132dc04afab7bd0eb8c201eb8dd6e80
Oreecaa <oreecaa@gmail.com>
2024-06-25 12:33:07 +0700

    - update mvvm block

--- lib/widget/text/input_formatters.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/services.dart';
-
-// 只允许输入两位小数
-TextInputFormatter doubleFormatter(int dot) =>
-    FilteringTextInputFormatter.allow(RegExp("^(\\d+)?\\.?\\d{0," + "$dot}"));
